81323d02416bef5f72d4b1b8d397f61239c490e3
depapp <depa.article@gmail.com>
2021-03-11 18:14:50 +0700

    update style & info

--- build.js
+++ build.js
@@ -8,27 +8,26 @@ const path = require('path')
 
 // Define options for Boxen
 const options = {
-  padding: 1,
-  margin: 1,
+  padding: 2,
+  margin: 2,
   borderStyle: 'round'
 }
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Depa Panjie Purnama'),
-  handle: chalk.white('depapp'),
-  work: chalk.white('Quality Engineer'),
-  github: chalk.gray('https://github.com/') + chalk.green('depapp'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('depapp'),
-  instagram: chalk.gray('https://instagram.com/') + chalk.cyan('depapp'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('depapp'),
-  web: chalk.cyan('https://depapp.github.io'),
-  labelWork: chalk.white.bold('       Work:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelInstagram: chalk.white.bold('     Instagram:'),
-  labelTwitter: chalk.white.bold('     Twitter:'),
-  labelWeb: chalk.white.bold('        Web:')
+  name: chalk.green.bold('Depa Panjie Purnama'),
+  handle: chalk.white.bold('depapp.github.io'),
+  work: chalk.white.bold('Quality Engineer'),
+  github: chalk.gray('github.com/') + chalk.white('depapp'),
+  linkedin: chalk.gray('linkedin.com/in/') + chalk.white('depapp'),
+  instagram: chalk.gray('instagram.com/') + chalk.white('depapp'),
+  twitter: chalk.gray('twitter.com/') + chalk.white('depapp'),
+  card: chalk.red.bold('npx') + chalk.green.bold(' depapp'),
+  labelWork: chalk.white.bold('     Work 👔 '),
+  labelGitHub: chalk.white.bold('   GitHub 🐙 '),
+  labelLinkedIn: chalk.white.bold(' LinkedIn 🔗 '),
+  labelTwitter: chalk.white.bold('  Twitter 🐦 '),
+  labelCard: chalk.white.bold('     Card 📇 ')
 }
 
 // Actual strings we're going to output
@@ -37,18 +36,17 @@ const heading = `${data.name} / ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const instagraming = `${data.labelInstagram}  ${data.instagram}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
-const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.card}`
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
+               newline + newline + newline + // Add one whole blank line
                working + newline + newline + // data.labelWork + data.work
                githubing + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
-               instagraming + newline + // data.labelInstagram + data.instagram
-               twittering + newline + // data.labelTwitter + data.twitter
-               webing // data.labelWeb + data.web
+               twittering +
+               newline + newline + newline + // Add one whole blank line
+               carding
 
 fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
